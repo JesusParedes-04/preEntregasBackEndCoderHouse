aba683eb4b6bc908ba48449c1d03af36a002895f
Jesús Paredes <jjesus.paredesl@gmail.com>
2023-03-06 22:46:07 -0300

    Revision ProductRouter

--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -6,70 +6,56 @@ class ProductManager {
         this.path = "./src/models/products.json"
     }
 
-readProducts = async () => {
+readProducts = async () => {  
 
     let products = await fs.readFile(this.path, "utf-8")
    return JSON.parse(products)
-
 }
 
-writeProducts = async (product) => {
+exist = async (id) => {  
+
+    let products = await this.readProducts();
+   return products.find(prod => prod.id === id)
+}
+
+writeProducts = async (product) => { 
 
     await fs.writeFile(this.path,JSON.stringify(product))
 
 }
 
-addProducts = async (product) => {
+addProducts = async (product) => {  
 let productsOld = await this.readProducts()
 product.id = nanoid()
 let productAll = [...productsOld, product]
 await this.writeProducts(productAll)
-return "producto agregado"
+return "product Added"
 }
 
-
-
-exist = async (id) => {
-    let products = await this.readProducts();
-   return products.find(prod => prod.id === id)
-}
-
-
-addProducts = async (product)=>{
-let productsOld = await this.readProducts()
-
-product.id = nanoid()
-let productAll = [...productsOld, product];
-   await this.writeProducts(productAll)
-   return "Producto Agregado"
-};
-
-
-getProducts = async ()=> {
+getProducts = async ()=> {  
 return await this.readProducts()
 }
 
-getProductsById = async(id)=>{
+getProductsById = async(id)=>{  
   let productById = await this.exist(id)
   if(!productById) return "product no founded"
   return productById
 }
 
 
-updateProducts = async (id,product)=>{
-    let productById = await this.exist(id)
-    if(!productById) return "product no founded"
-
+updateProducts = async (id,product)=>{ 
+let productById = await this.exist(id)
+if(!productById) return "product no founded"
 await this.deleteProducts(id)
-let productsOld = await this.readProducts()
-let products = [{...product, id: id}, ...productsOld]
+let productOld = await this.readProducts()
+let products = [{...product, id: id}, ...productOld]
 await this.writeProducts(products)
 return "Product Updated"
-}
+        }
 
 
-deleteProducts = async (id) => {
-    let products = await this.readProducts();
+deleteProducts = async (id) => { 
+let products = await this.readProducts();
 let existProducts = products.some(prod => prod.id === id)
 if(existProducts){
 let filterProducts = products.filter(prod => prod.id !=id)
